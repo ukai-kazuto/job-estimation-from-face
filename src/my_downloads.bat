d934c710f47def40bb6f9ef616fe1d74dcb82d12
ukai-kazuto <ukai.kazuto@isid.co.jp>
2018-06-27 16:20:35 +0900

    ソースを追加

---
+++ src/my_downloads.bat
@@ -0,0 +1,63 @@
+echo "CEO" >> log.txt
+echo %date%-%time% >> log.txt
+python google_images_download.py -k –x]—²•¶
+python google_images_download.py -k ‘·³‹`
+python google_images_download.py -k ŽO–Ø’J_Žj
+python google_images_download.py -k –öˆä³
+python google_images_download.py -k ‹{âŠw
+python google_images_download.py -k “c’†—Ç˜a
+python google_images_download.py -k Îˆä’¼
+
+echo "ƒAƒCƒhƒ‹" >> log.txt
+echo %date%-%time% >> log.txt
+python google_images_download.py -k ‰ª“cƒˆê
+python google_images_download.py -k “ñ‹{˜a–ç
+python google_images_download.py -k ŸNˆäãÄ
+python google_images_download.py -k ¼–{
+python google_images_download.py -k Žè‰z—S–ç
+python google_images_download.py -k ’·£’q–ç
+python google_images_download.py -k ŽR“c—Á‰î
+
+echo "‰ÌŽè" >> log.txt
+echo %date%-%time% >> log.txt
+python google_images_download.py -k ¯–ìŒ¹
+python google_images_download.py -k ‚‹´—D
+python google_images_download.py -k •½ˆäŒ˜
+python google_images_download.py -k ˆî—t_Žu
+python google_images_download.py -k ê Œ´Œh”V
+python google_images_download.py -k ŒK“c‰À—S
+python google_images_download.py -k ‹v•Û“c—˜L
+
+echo "”o—D" >> log.txt
+echo %date%-%time% >> log.txt
+python google_images_download.py -k È•v–Ø‘
+python google_images_download.py -k ŽRú±Œ«l
+python google_images_download.py -k “Œo¹‘å
+python google_images_download.py -k ’|“à—Á^
+python google_images_download.py -k âŒûŒ’‘¾˜Y
+python google_images_download.py -k ¼“‡Gr
+python google_images_download.py -k ˆ»–ì„
+
+echo "Œ|l" >> log.txt
+echo %date%-%time% >> log.txt
+python google_images_download.py -k Œ´“c‘×‘¢
+python google_images_download.py -k ã“cW–ç
+python google_images_download.py -k “Œ–ì_“ñ
+python google_images_download.py -k “à‘ºŒõ—Ç
+python google_images_download.py -k oì“N˜Y
+python google_images_download.py -k ‰ª‘º—²Žj
+python google_images_download.py -k Œã“¡‹PŠî
+
+echo "­Ž¡‰Æ" >> log.txt
+echo %date%-%time% >> log.txt
+python google_images_download.py -k –ƒ¶‘¾˜Y
+python google_images_download.py -k ˆÀ”{WŽO
+python google_images_download.py -k –ì“c‰À•F
+python google_images_download.py -k ›’¼l
+python google_images_download.py -k ”µŽR—R‹I•v
+python google_images_download.py -k •Ÿ“cN•v
+python google_images_download.py -k ¬òƒˆê˜Y
+
+echo "FINISH" >> log.txt
+echo %date%-%time% >> log.txt
+pause 
---
+++ src/predict.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from keras.applications.vgg16 import VGG16
+from keras.models import Sequential, Model
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Input, Activation, Dropout, Flatten, Dense
+from keras.preprocessing import image
+import numpy as np
+
+if len(sys.argv) != 2:
+    print("usage: python predict.py [filename]")
+    sys.exit(1)
+
+filename = sys.argv[1]
+print('input:', filename)
+
+result_dir = 'results'
+
+img_height, img_width = 150, 150
+
+# 学習済みの重みをロード
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, input_shape=(150, 150, 1)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+
+
+model.load_weights(os.path.join(result_dir, 'smallcnn.h5', grayscale=True))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+# model.summary()
+
+# 画像を読み込んで4次元テンソルへ変換
+img = image.load_img(filename, target_size=(img_height, img_width))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+
+# 学習時にImageDataGeneratorのrescaleで正規化したので同じ処理が必要！
+# これを忘れると結果がおかしくなるので注意
+x = x / 255.0
+
+# print(x)
+# print(x.shape)
+
+# クラスを予測
+# 入力は1枚の画像なので[0]のみ
+pred = model.predict(x)[0]
+print(pred)
